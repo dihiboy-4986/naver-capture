74e83efabcccd316984fd58262c624a72671c37b
H KIM <dihiboy@gmail.com>
2026-04-16 13:37:40 +0900

    add font file

--- render-build.sh
+++ render-build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-apt-get update
-apt-get install -y fonts-nanum fonts-nanum-coding fonts-nanum-extra
+mkdir -p /usr/local/share/fonts/nanum
+cp NanumGothic.ttf /usr/local/share/fonts/nanum/
 fc-cache -fv
 playwright install chromium
